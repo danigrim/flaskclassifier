79b126586f3e30c8039ef1641fa9c14a89e66061
Daniella Grimberg <57309621+danigrim@users.noreply.github.com>
2021-01-10 16:55:43 +0200

    Add files via upload

---
+++ model_predict_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+import json
+
+filename = '/rfc_model.pkl'
+app = Flask(__name__)
+model = pickle.load(open(filename,'rb'))
+
+
+@app.route('/predict_single', methods=['GET'])
+def predict_single():
+    try:
+        df = pd.DataFrame.from_dict({feat: [float(request.args.get(feat))] for feat in request.args})
+        prediction = model.predict(df)[0]
+    except:
+        return "URL not properly formatted. Try format: /predict_single?age=10&sex=1&trestbps=140&chol=193&fbs=0.0&restecg=0.0&thalach=145&exang=1.0&oldpeak=1.0&slope=2.0"
+    return str(prediction)
+
+
+@app.route('/predict_many', methods=['POST'])
+def predict_many():
+    try:
+        content = request.json
+        df = pd.DataFrame(content)
+        prediction = model.predict(df)
+    except:
+        return jsonify({'error':"JSON wrong format" })
+    return jsonify({'prediction': list(prediction)})
+
+
+if __name__ == "main":
+    app.run()
