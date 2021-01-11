18fdaf9cd1775fea5af55dcbff88b2efaa51c3d2
danigrim <danigrim@stanford.edu>
2021-01-11 19:40:36 +0200

    fix/exception_handling

--- flaskclassifier.py
+++ flaskclassifier.py
@@ -29,7 +29,7 @@ def predict_single():
 def predict_many():
     # try:
     content = request.json
-    df = pd.DataFrame(content)
+    df = pd.DataFrame(pd.read_json(content, orient='records'))
     prediction = model.predict(df)
     # except:
     #     return jsonify({'error':"JSON wrong format" })
