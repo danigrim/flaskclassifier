44a9785f675b1e8a20a5bde3229cf789a96ecbb3
danigrim <danigrim@stanford.edu>
2021-01-11 16:27:17 +0200

    fix-json

--- flaskclassifier.py
+++ flaskclassifier.py
@@ -29,7 +29,7 @@ def predict_single():
 def predict_many():
     try:
         content = request.json
-        df = pd.DataFrame(pd.read_json(content, orient='records'))
+        df = pd.DataFrame(content)
         prediction = model.predict(df)
     except:
         return jsonify({'error':"JSON wrong format" })
