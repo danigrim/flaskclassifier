48c9e60c1de9381d0f661c90b517b89f9cbe11d0
danigrim <danigrim@stanford.edu>
2021-01-11 22:56:21 +0200

    json_normalize

--- flaskclassifier.py
+++ flaskclassifier.py
@@ -29,7 +29,7 @@ def predict_single():
 def predict_many():
     try:
         content = request.json
-        df = pd.read_json(content)
+        df = pd.json_normalize(content)
         df = pd.DataFrame(df)
         prediction = model.predict(df)
     except:
