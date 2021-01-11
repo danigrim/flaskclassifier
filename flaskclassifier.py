e374fed703a3d30f295fb1f7524f39bb2d7cdc25
danigrim <danigrim@stanford.edu>
2021-01-11 22:36:37 +0200

    json_loads

--- flaskclassifier.py
+++ flaskclassifier.py
@@ -29,7 +29,7 @@ def predict_single():
 def predict_many():
     try:
         content = request.json
-        df = json.loads(content)
+        df = pd.read_json(content)
         df = pd.DataFrame(df)
         prediction = model.predict(df)
     except:
